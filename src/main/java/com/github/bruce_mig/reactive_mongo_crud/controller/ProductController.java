d7f634187a830bd4c056644103e9bc0665304ba3
bruce-mig <brucetmig@icloud.com>
2024-12-16 16:26:55 +0200

    added response entities

--- src/main/java/com/github/bruce_mig/reactive_mongo_crud/controller/ProductController.java
+++ src/main/java/com/github/bruce_mig/reactive_mongo_crud/controller/ProductController.java
@@ -2,7 +2,9 @@ package com.github.bruce_mig.reactive_mongo_crud.controller;
 
 import com.github.bruce_mig.reactive_mongo_crud.dto.ProductDto;
 import com.github.bruce_mig.reactive_mongo_crud.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -23,8 +25,10 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public Mono<ProductDto> getProductById(@PathVariable String id){
-        return service.getProductById(id);
+    public Mono<ResponseEntity<ProductDto>> getProductById(@PathVariable String id){
+        return service.getProductById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/price-range")
@@ -35,20 +39,26 @@ public class ProductController {
     }
 
     @PostMapping
-    public Mono<ProductDto> saveProduct(@RequestBody Mono<ProductDto> productDtoMono){
-        return service.saveProduct(productDtoMono);
+    public Mono<ResponseEntity<ProductDto>> saveProduct(@RequestBody Mono<ProductDto> productDtoMono){
+        return service.saveProduct(productDtoMono)
+                .map(savedProduct -> ResponseEntity.status(HttpStatus.CREATED).body(savedProduct));
+
     }
 
     @PutMapping("/update/{id}")
-    public Mono<ProductDto> updateProduct(
+    public Mono<ResponseEntity<ProductDto>> updateProduct(
             @RequestBody Mono<ProductDto> productDtoMono,
             @PathVariable String id){
-        return service.updateProduct(productDtoMono, id);
+        return service.updateProduct(productDtoMono, id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/delete/{id}")
-    public Mono<Void> deleteProductById(@PathVariable String id){
-        return service.deleteProductById(id);
+    public Mono<ResponseEntity<Void>> deleteProductById(@PathVariable String id){
+        return service.deleteProductById(id)
+                .thenReturn(ResponseEntity.noContent().<Void>build())
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
 }
